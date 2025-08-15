cc57e314c1e60e3e4e71254e6afb797b8e019cf6
sourab6058 <sourabkumar1236@gmail.com>
2025-08-15 17:03:14 +0530

    Update Next.js configuration and add deployment script

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
 
-export default nextConfig;
+export default {
+  output: "export",
+  basePath: isProd ? "/nextjs-app" : "",
+  assetPrefix: isProd ? "/nextjs-app/" : "",
+};
